681c4ce463ed5615add0f84089f6e3e7808405c4
Kelby Hubbard <krhubbard2@alaska.edu>
2020-08-31 17:09:55 -0800

    Added definition of member functions

--- Project1/productorder.cpp
+++ Project1/productorder.cpp
@@ -8,3 +8,36 @@
 // Product Order: product name, number of items ordered
 
 #include "productorder.h" // For class ProductOrder
+
+
+// *********************************************************************
+// class ProductOrder - Definitions of member functions
+// *********************************************************************
+
+// ProductOrder::getName
+// (See header for docs.)
+void ProductOrder::getName(string & name) const
+{
+  name = _name;
+}
+
+// ProductOrder::setName
+// (See header for docs.)
+void ProductOrder::setName(string name)
+{
+  _name = name;
+}
+
+// ProductOrder::getNumber
+// (See header for docs.)
+void ProductOrder::getNumber(int & inventory) const
+{
+  inventory = _numb;
+}
+
+// ProductOrder::setNumber
+// (See header for docs.)
+void ProductOrder::setNumber(int inventory)
+{
+  _numb = inventory;
+}
