274982948b8c309ef244f4642968bc8b9a499fd8
Kelby <krhubbard2@alaska.edu>
2020-09-18 10:25:43 -0800

    Added basis for Project3

---
+++ Project3/dp3.cpp
@@ -0,0 +1,27 @@
+// dp3.cpp  SKELETON
+// Glenn G. Chappell
+// 2020-09-17
+//
+// For CS 311 Fall 2020
+// Source for Project 3 functions
+
+#include "dp3.h"       // For Project 3 prototypes & templates
+
+#include <functional>
+using std::function;
+
+
+void didItThrow(const function<void()> & ff,
+                bool & threw)
+{
+    // TODO: Write this!!!
+}
+
+
+int gcd(int a,
+        int b)
+{
+    return 42;  // Dummy return
+    // TODO: Write this!!!
+}
+
