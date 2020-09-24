0fc5c5732186f61b428758e513d957c74c35d869
Kelby Hubbard <krhubbard2@alaska.edu>
2020-09-24 13:17:34 -0800

    Finished

--- Project3/dp3.cpp
+++ Project3/dp3.cpp
@@ -1,16 +1,24 @@
-// dp3.cpp  SKELETON
-// Glenn G. Chappell
-// 2020-09-17
-//
+// dp3.cpp
+// Kelby Hubbard
+// Started: 2020-09-18
+// Updated: 2020-09-24
+
 // For CS 311 Fall 2020
-// Source for Project 3 functions
+// Header for Project 3 functions
+
+// CREDIT:
+// All code below is written by me (Kelby Hubbard)
+// This code was built off of the skeleton code written and styled by
+// Glenn G. Chappell from dp3.cpp which can be
+// seen at https://github.com/ggchappell/cs311-2020-03
 
 #include "dp3.h"       // For Project 3 prototypes & templates
 
 #include <functional>
 using std::function;
 
-
+// void didItThrow
+// See dp3.h for function information
 void didItThrow(const function<void()> & ff,
                 bool & threw)
 {
@@ -28,16 +36,17 @@ void didItThrow(const function<void()> & ff,
     return;
 }
 
-
+// int gcd
+// See dp3.h for function information
 int gcd(int a,
         int b)
 {
   if (a == 0) return b;
   else if (a > b)
   {
-    gcd(b, a);
+    return gcd(b, a);
   }
   else{
-    gcd(b % a, a);
+    return gcd(b % a, a);
   }
 }
--- Project3/dp3.h
+++ Project3/dp3.h
@@ -1,10 +1,16 @@
 // dp3.h
 // Kelby Hubbard
 // Started: 2020-09-18
-// Updated: 2020-09-21
+// Updated: 2020-09-24
 
 // For CS 311 Fall 2020
 // Header for Project 3 functions
+
+// CREDIT:
+// All code below is written by me (Kelby Hubbard)
+// This code was built off of the skeleton code written and styled by
+// Glenn G. Chappell from dp3.h which can be
+// seen at https://github.com/ggchappell/cs311-2020-03
 
 #ifndef FILE_DP3_H_INCLUDED
 #define FILE_DP3_H_INCLUDED
@@ -14,7 +20,8 @@
 #include <functional>  // For std::function
 #include <stdexcept>   // For std::out_of_range
 
-
+// Looks through a Singly Linked List
+// Throws an error exception (out_of_range) if incorrect entry is inputted.
 template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> * head,
                  std::size_t index)
@@ -37,10 +44,15 @@ ValueType lookup(const LLNode<ValueType> * head,
 
 
 // Implementation in source file
+// Pass a function pointer or object that acts like a function
+// If the function throws, sets parameter threw to true and passes
+// the exception on to the caller. If function doesn't throw, sets
+// parameter threw to false and returns normally
 void didItThrow(const std::function<void()> & ff,
                 bool & threw);
 
-
+// Takes iterators as paramters and processes a range of data
+// Returns true if data is sorted in ascending order. Returns false otherwise.
 template <typename FDIter>
 bool checkSorted(FDIter first,
                  FDIter last)
@@ -57,6 +69,7 @@ bool checkSorted(FDIter first,
 
 
 // Implementation in source file
+// Returns the GCD of two nonnegative integers using recursive methods.
 int gcd(int a,
         int b);
 
