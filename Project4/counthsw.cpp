00a9ca05f6900d054d9b8774150868869eba7a2c
Kelby Hubbard <krhubbard2@alaska.edu>
2020-09-28 14:48:43 -0800

    Added basis of file structure

---
+++ Project4/counthsw.cpp
@@ -0,0 +1,7 @@
+// counthsw.cpp
+// Kelby Hubbard
+// Started: 2020-09-28
+// Updated: 2020-09-28
+
+// For CS 311 Fall 2020
+// Source file for Project 4
---
+++ Project4/counthsw.h
@@ -0,0 +1,15 @@
+// counthsw.h
+// Kelby Hubbard
+// Started: 2020-09-28
+// Updated: 2020-09-28
+
+// For CS 311 Fall 2020
+// Header for Project 4
+
+#ifndef FILE_COUNTHSW_H_INCLUDED
+#define FILE_COUNTHSW_H_INCLUDED
+
+
+
+
+#endif // #ifndef FILE_COUNTHSW_H_INCLUDED
