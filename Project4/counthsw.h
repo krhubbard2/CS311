a0c1c28a5330a94cdbb64ded286fb3eb8e9eb352
Kelby Hubbard <krhubbard2@alaska.edu>
2020-09-28 16:26:32 -0800

    int countHSW prototype added

--- Project4/counthsw.h
+++ Project4/counthsw.h
@@ -9,7 +9,10 @@
 #ifndef FILE_COUNTHSW_H_INCLUDED
 #define FILE_COUNTHSW_H_INCLUDED
 
-
+int countHSW(int dim_x, int dim_y,
+             int hole_x, int hole_y,
+             int start_x, int start_y,
+             int finish_x, int finish_y);
 
 
 #endif // #ifndef FILE_COUNTHSW_H_INCLUDED
