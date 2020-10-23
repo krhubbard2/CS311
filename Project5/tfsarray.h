a9218406ff670b9d9fc60cd8e8c12a1c9e89d8d6
Kelby Hubbard <krhubbard2@alaska.edu>
2020-10-22 20:29:31 -0800

    Finished skeleton.

--- Project5/tfsarray.h
+++ Project5/tfsarray.h
@@ -24,6 +24,14 @@
 // class TFSArray - Class definition
 // *********************************************************************
 
+// class TFSArray
+// Templated Frightfully Smart Array.
+// Resizable, copyable/movable, exception-safe.
+// Invariants:
+//	0 <= _size <= _capacity
+//	_data points to an array of value_type, allocated with new [],
+//	owned by *this, holding _capacity value_type values -- UNLESS
+//	_capacity == 0, in which case _data may be nullptr.
 template <typename T>
 class TFSArray{
 
@@ -55,10 +63,40 @@ public:
   	:_capacity(std::max(size, size_type(DEFAULT_CAP))),
   	  // _capacity must be declared before _data
   	_size(size),
-  	_data(_cpacity == 0 ? nullptr : new value_type[_capacity])
+  	_data(_capacity == 0 ? nullptr : new value_type[_capacity])
   {}
 
+  // Copy constructor
+  // Strong Guarantee
+  // Pre:
+  //	  A valid TFSArray object to copy
+  TFSArray(const TFSArray & other):
+  	_size(other.size()),
+  	_data(new value_type[other._capacity]),
+  	_capacity(other._capacity)
+  {
+    try
+    {
+      std::copy(other.begin(), other.end(), begin());
+    }
+    catch(...)
+    {
+      this->~TFSArray();
+      throw;
+    }
+  }
+  
+  // Move constructor
+  // No-Throw Guarantee
+  TFSArray(TFSArray && other);
 
+  // Copy assignment
+  // 
+  TFSArray & operator=(const TFSArray & other);
+  
+  // Move assignment
+  // No-Throw Guarantee
+  TFSArray & operator=(TFSArray && other) noexcept;
 
   // Deconstructor
   // No-Throw Guarantee
@@ -66,6 +104,92 @@ public:
   {
     delete [] _data;
   }
+ 
+// ***** TFSarray: General Public Operators *****
+public:
+
+  // operator[] - non-const & const
+  // Pre:
+  //	 
+  // No-Throw Guarantee
+  value_type & operator[](size_type index) noexcept
+  {
+    return _data[index];
+  }
+  const value_type & operator[](size_type index) const noexcept
+  {
+    return _data[index];
+  }
+  
+// ***** TFSArray: General Public Functions *****
+public:
+
+  // size
+  // No-Throw Guarantee 
+  size_type size() const noexcept
+  {
+    return _size;
+  }
+  
+  // empty
+  // No-Throw Guarantee
+  bool empty() const noexcept
+  {
+    return size() == 0;
+  }
+  
+  // begin - non-const & const
+  // No-Throw Guarantee
+  iterator begin() noexcept
+  {
+    return _data;
+  }
+  const_iterator begin() const noexcept
+  {
+    return _data;
+  }
+
+  // end - non-const & const
+  // No-Throw Guarantee
+  iterator end() noexcept
+  {
+    return begin() + size();
+  }
+  const_iterator end() const noexcept
+  {
+    return begin() + size();
+  }
+
+  // resize
+  // ??? Guarantee
+  void resize(size_type newsize);
+
+  // insert
+  // ??? Guarantee
+  iterator insert(iterator pos,
+                  const value_type & item);
+
+  // erase
+  // ??? Guarantee
+  iterator erase(iterator pos);
+
+  // push_back
+  // ??? Guarantee
+  void push_back(const value_type & item)
+  {
+    insert(end(), item);
+  }
+
+  // pop_back
+  // ??? Guarantee
+  void pop_back()
+  {
+    erase(end()-1);
+  }
+
+  // swap
+  // No-Throw Guarantee
+  void swap(TFSArray & other) noexcept;  
   
 // ***** TFSArray: Data Members *****
 private:
