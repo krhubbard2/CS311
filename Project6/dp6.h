3022bfb23b153fc83efdb859695f5fed2754ebf2
Kelby Hubbard <krhubbard2@alaska.edu>
2020-10-30 13:20:00 -0800

    Added prototypes and comments.

--- Project6/dp6.h
+++ Project6/dp6.h
@@ -12,3 +12,37 @@
 //
 //
 
+#ifndef FILE_DP6_H_INCLUDED
+#define FILE_DP6_H_INCLUDED
+
+#include "llnode2.h"	
+#include <memory>
+
+// reverseList
+// ??? Discription
+// Pre:
+//		
+// ??? Guarantee
+template<typename ValType>
+void reverseList(unique_ptr<LLNode2<ValType>> & head)
+{
+
+}
+
+// **********************************************************************
+// class LLMap - Class definition
+// **********************************************************************
+// class LLMap
+// Templated linked list map
+// Invariants:
+//		???
+// Type requirements:
+//		???
+template <typename Key, typename Data>
+class LLMap {
+
+
+
+}; // End class LLMap
+
+#endif // FILE_DP6_H_INCLUDED
