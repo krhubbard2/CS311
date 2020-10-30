98c935ce88dd46c3228cc4445fb996bb00b3a904
Kelby <krhubbard2@alaska.edu>
2020-10-30 10:38:43 -0800

    Added additional needed files and comments

---
+++ Project6/dp6.h
@@ -0,0 +1,14 @@
+// dp6.h
+// Kelby Hubbard, Rose Peters, and Yali Wang
+// Started: 2020-10-30
+// Updated: 2020-10-30
+
+// For CS 311 Fall 2020
+// Header for Project 6 functions
+
+
+// CREDIT:
+//
+//
+//
+
